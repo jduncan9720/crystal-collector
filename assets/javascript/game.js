1a8e393a08d7cf738ed5130f60bb75a7b3491fa8
Joshua Duncan <jduncan97@gmail.com>
2020-03-19 14:30:38 -0600

    Added additional javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,57 @@
+//Global variables for wins=0, losses=0, crystal numbers, current score, and target numbers 
+var wins = 0;
+var losses = 0;
+var crystal1;
+var crystal2;
+var crystal3;
+var crystal4;
+var currentScore = 0;
+var targetScore = 0;
+
+//Call setup function
+setup();
+
+//Function that sets up a new game
+function setup(){
+    //Change target number
+    targetScore = Math.floor(Math.random() * 40) + 20;
+    //Change current score
+    currentScore = 0;
+    //Change crystal numbers
+    crystal1 = Math.floor(Math.random() * 10);
+    crystal2 = Math.floor(Math.random() * 10);
+    crystal3 = Math.floor(Math.random() * 10);
+    crystal4 = Math.floor(Math.random() * 10);
+    if (crystal1 % 2 == 0) {
+        crystal1++;
+    }
+    //Display target score,wins,losses,and current score
+    document.getElementById("target-number").innerHTML = targetScore;
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("losses").innerHTML = losses;
+    document.getElementById("score").innerHTML = currentScore;
+}
+    
+//Onclick function that says on click of any crystal, add their number to the player score
+    $("#bluegem").on("click", function() {
+        alert("Help Me")
+
+    });
+
+    
+
+
+    //Check if current score is less than, equal to, or greater than target number
+        //If less than, do nothing
+        //If greater than, run lose function
+        //If equal to, run win function
+
+//A winner is you function
+    //Display winner text
+    //Increment wins
+    //Run new game function
+
+//You lose function
+    //Display loser text
+    //Increment losses
+    //Run new game function
